86fceb5ca1acd2a790ff04f4c29dce7093ec6175
zestx5 <buyankov.ilya@gmail.com>
2023-07-25 12:53:44 +0300

    add: basic menu and input handler

---
+++ Books/UserInterface/MenuHandler.cs
@@ -0,0 +1,80 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+
+namespace Books.UserInterface;
+
+public class MenuHandler
+{
+    public static void Start()
+    {
+        string menu = """
+        =============== Menu ===============
+        1. Add book 
+        2. View all books
+        3. Edit book info
+        4. Change status
+        5. Delete book
+        6. Exit
+        ====================================
+        """;
+
+        Console.WriteLine(menu);
+        Console.WriteLine();
+
+        var userSelect = UserInputHandler.GetIntegerInput("Input: ");
+        Resolve(userSelect)();
+    }
+
+    private static Action Resolve(int input) => input switch
+    {
+        1 => CreateBook,
+        2 => DisplayAllBooks,
+        3 => EditBook,
+        4 => ChangeStatus,
+        5 => DeleteBook,
+        6 => Exit,
+        _ => InvalidOption
+    };
+
+    private static void InvalidOption()
+    {
+        Console.WriteLine("Wrong input");
+        Start();
+    }
+
+    private static void DisplayAllBooks()
+    {
+        Console.WriteLine("WIP");
+        Start();
+    }
+
+    private static void EditBook()
+    {
+        Console.WriteLine("WIP");
+        Start();
+    }
+
+    private static void ChangeStatus()
+    {
+        Console.WriteLine("WIP");
+        Start();
+    }
+
+    private static void DeleteBook()
+    {
+        Console.WriteLine("WIP");
+        Start();
+    }
+
+    private static void Exit()
+    {
+        Environment.Exit(0);
+    }
+
+    private static void CreateBook()
+    {
+        Console.WriteLine("WIP");
+        Start();
+    }
+
+}
---
+++ Books/UserInterface/UserInputHandler.cs
@@ -0,0 +1,54 @@
+﻿
+namespace Books.UserInterface;
+
+public static class UserInputHandler
+{
+    public static string? GetStringInput(string prompt)
+    {
+        Console.WriteLine(prompt);
+        return Console.ReadLine();
+    }
+
+    public static int GetIntegerInput(string prompt)
+    {
+        while (true)
+        {
+            Console.Write(prompt);
+            string? input = Console.ReadLine();
+
+            if (int.TryParse(input, out int result))
+            {
+                return result;
+            }
+
+            Console.WriteLine("Invalid input. Please enter a valid integer.");
+        }
+    }
+
+    public static bool GetYesNoInput(string prompt)
+    {
+        while (true)
+        {
+            Console.Write(prompt + " (Yes/No): ");
+            string? input = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("Invalid input. Please enter either 'Yes' or 'No'.");
+                continue;
+            }
+
+            if (input.Equals("Yes", StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+            else if (input.Equals("No", StringComparison.OrdinalIgnoreCase))
+            {
+                return false;
+            }
+
+            Console.WriteLine("Invalid input. Please enter either 'Yes' or 'No'.");
+        }
+    }
+
+}
